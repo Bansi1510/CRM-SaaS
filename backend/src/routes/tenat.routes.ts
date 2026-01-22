f5e3055d7523cbe58feb07c3d78c74f3f426862b
prajapati bansi <prajapatibansi1510@gmail.com>
2026-01-22 22:10:37 +0530

    user controller start making

--- backend/src/routes/tenat.routes.ts
+++ backend/src/routes/tenat.routes.ts
@@ -1,9 +1,11 @@
 import express, { Router } from "express";
 import isSuperAdmin from "../middlewares/isSuperAdmin.middleware.js";
-import { CreateTenant, getTenantById } from "../controllers/tenat.controller.js";
+import { CreateTenant, getTenantById, updateTenant } from "../controllers/tenat.controller.js";
 
 const TenatRouter: Router = express.Router();
 
 TenatRouter.post("/create", isSuperAdmin, CreateTenant)
 TenatRouter.get("/:tenant_id", isSuperAdmin, getTenantById);
+TenatRouter.put("/:tenant_id/update", isSuperAdmin, updateTenant);
+
 export default TenatRouter;
